4e6980938e710a4dda993b0c5174291c680e8a98
mucahitkosgen <e160503138@stud.tau.edu.tr>
2020-05-20 01:10:20 +0300

    deneme

--- projee/src/mainpanel.java
+++ projee/src/mainpanel.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 import java.sql.*;
 
 import static java.sql.DriverManager.getConnection;
-
+// personal bilgilerini hallettim burada.
 public class mainpanel extends JFrame {
     private JPanel mainpanel;
     private JLabel adisoyadiLabel;
@@ -18,6 +18,7 @@ public class mainpanel extends JFrame {
     private JTextField tarihtextField1;
     private JButton okbutton1;
     private JButton silbutton1;
+    private JButton guncellebutton1;
 
     public static Connection connect=null;
     public static Statement statement=null;
@@ -25,65 +26,84 @@ public class mainpanel extends JFrame {
     public static String url="jdbc:mysql://localhost:3306/connect_mysql_database?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey";
     public  static String user="root",pass="Kule1845";
 
-    public mainpanel(String title) {
+
+    public mainpanel(String title){
         super(title);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainpanel);
+        silbutton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                String x = adisoyaditextField1.getText();
+                String y = seviyetextField1.getText();
+                String z = tarihtextField1.getText();
+                try {
+
+                    statement.executeUpdate("DELETE FROM mainpanel where adisoyadi='"+x+"'");
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        //silme içinde addAction yapacağız .remove değil
         okbutton1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                String x=adisoyaditextField1.getText();
-                String y=seviyetextField1.getText();
-                String z=tarihtextField1.getText();
+                String x = adisoyaditextField1.getText();
+                String y = seviyetextField1.getText();
+                String z = tarihtextField1.getText();
 
                 try {
-                    statement.executeUpdate("INSERT INTO personal_information VALUES ('"+x+"','"+y+"','"+z+"')");
+                    statement.executeUpdate("INSERT INTO mainpanel VALUES ('" + x + "','" + y + "','" + z + "')");
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
+
         });
-        silbutton1.removeActionListener(new ActionListener() {
+        guncellebutton1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                String x=adisoyaditextField1.getText();
-                String y=seviyetextField1.getText();
-                String z=tarihtextField1.getText();
+                String x = adisoyaditextField1.getText();
+                String y = seviyetextField1.getText();
+                String z = tarihtextField1.getText();
 
                 try {
-                    statement.executeUpdate("DELETE FROM personal_information where adisoyadi='"+x+"'");
+                    statement.executeUpdate("UPDATE mainpanel set seviye='"+y+"',tarih='"+z+"'where adisoyadi='"+x+"'");
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
         });
 
+
+
     }
-
-
 
     public static void main(String[] args) {
-        JFrame frame=new mainpanel("Gözetim Muayene Raporu");
+        JFrame frame = new mainpanel("Gözetim Muayene Raporu");
         frame.setVisible(true);
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connect = DriverManager.getConnection(url, user, pass);
-             statement = connect.createStatement();
-             ResultSet resultSet = statement.executeQuery("select * from personal_information");
-                while (resultSet.next()) {
-                    String adisoyadi = resultSet.getString(1);
-                    String seviye = resultSet.getString(2);
-                    String tarih = resultSet.getString(3);
-                    System.out.println(adisoyadi + " " + seviye + " " + tarih);
-        }
-    } catch (SQLException e){
+        try {
+            // Class.forName("com.mysql.cj.jdbc.Driver");
+            //connect = DriverManager.getConnection(url, user, pass);
+            Connection connect = database.getConnection();
+            statement = connect.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from mainpanel");
+            while (resultSet.next()) {
+                String adisoyadi = resultSet.getString(1);
+                String seviye = resultSet.getString(2);
+                String tarih = resultSet.getString(3);
+                System.out.println(adisoyadi + " " + seviye + " " + tarih);
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
-        }catch (ClassNotFoundException e){
-            e.printStackTrace();
-        }
+        }//catch (ClassNotFoundException e) {
+           // e.printStackTrace();
+
+        //}
 
 
-    }
-
-
-}
+    }}
