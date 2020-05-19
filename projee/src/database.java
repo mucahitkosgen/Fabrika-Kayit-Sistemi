edcc995864b3aeb300412d48b2a79b50aeff55ef
mucahitkosgen <e160503138@stud.tau.edu.tr>
2020-05-20 01:03:59 +0300

    deneme

--- projee/src/database.java
+++ projee/src/database.java
@@ -18,7 +18,7 @@ public class database {
                 System.out.println("fail");
             }
         } catch (ClassNotFoundException e) {
-            System.out.println("nottt found");
+            System.out.println("not found");
         }
 
 
