0b170f6b387f2a9cd64bbc83563b62fa782ece6f
mucahitkosgen <e160503138@stud.tau.edu.tr>
2020-05-20 01:06:01 +0300

    deneme

--- projee/src/yonetici.java
+++ projee/src/yonetici.java
@@ -8,13 +8,15 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.*;
 
-public class yonetici {
+public class yonetici extends JFrame {
     private JPanel yonetici;
     private JLabel yoneticiisimLabel;
     private JTextField yoneticiisimtextField1;
     private JLabel sıfreLabel;
     private JPasswordField sıfrepasswordField1;
     private JButton okbutton1;
+    private JButton silbutton1;
+    private JButton guncellebutton1;
 
     public static Connection connect = null;
     public static Statement statement = null;
@@ -22,7 +24,11 @@ public class yonetici {
     public static String url = "jdbc:mysql://localhost:3306/connect_mysql_database?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey";
     public static String user = "root", pass = "Kule1845";
 
-    public yonetici(String title) {
+
+    public yonetici(String title){
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(yonetici);
 
 
         okbutton1.addActionListener(new ActionListener() {
@@ -32,39 +38,67 @@ public class yonetici {
                 String y = sıfrepasswordField1.getText();
 
                 try {
-                    statement.executeUpdate("INSERT INTO yonetici VALUES ('" + x + "','" + y + "',)");
+                    statement.executeUpdate("INSERT INTO yonetici VALUES ('" + x + "','" + y + "')");
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
 
         });
+        silbutton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String x=yoneticiisimtextField1.getText();
+                String y=sıfrepasswordField1.getText();
+
+
+                try{
+                    statement.executeUpdate("DELETE FROM yonetici where yoneticiisim='"+x+"'");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        guncellebutton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String  x=yoneticiisimtextField1.getText();
+                String y=sıfrepasswordField1.getText();
+
+                try{
+                    statement.executeUpdate("UPDATE yonetici set sifre='"+y+"' where yoneticiisim='"+x+"'");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
 
     }
 
 
 
 
-
     public static void main(String[] args) {
-        JFrame frame = new musteri("Yönetici giriş");
+        JFrame frame = new yonetici("Yönetici giriş");
         frame.setVisible(true);
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connect = DriverManager.getConnection(url, user, pass);
+            //Class.forName("com.mysql.cj.jdbc.Driver");
+            //connect = DriverManager.getConnection(url, user, pass);
+            Connection connect=database.getConnection();
             statement = connect.createStatement();
             ResultSet resultSet = statement.executeQuery("select * from yonetici");
             while (resultSet.next()) {
                 String yoneticiisim = resultSet.getString(1);
                 String sifre = resultSet.getString(2);
-                String testyeri = resultSet.getString(3);
                 System.out.println(yoneticiisim + " " + sifre + " " );
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        } //catch (ClassNotFoundException e) {
+            //e.printStackTrace();
+        //}
 
     }
 }
