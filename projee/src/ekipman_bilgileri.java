5ff0c62dbd6a1b4f17eb6e3e20da61ec52412828
mucahitkosgen <e160503138@stud.tau.edu.tr>
2020-05-22 05:55:03 +0300

    deneme

--- projee/src/ekipman_bilgileri.java
+++ projee/src/ekipman_bilgileri.java
@@ -20,6 +20,26 @@ public class ekipman_bilgileri extends JFrame {
     private JButton okbutton1;
     private JButton silbutton1;
     private JButton guncellebutton1;
+    private JLabel kutupmesafesiLabel;
+    private JTextField kutupmesafesitextField1;
+    private JLabel miknatislamateknigiLabel;
+    private JTextField miknatislamateknigitextField1;
+    private JLabel uvisiksideetiLabel;
+    private JTextField uvisiksiddetitextField1;
+    private JLabel isikmesafesiLabel;
+    private JTextField isikmesafesitextField1;
+    private JLabel isiksiddetiLabel;
+    private JTextField isiksiddetitextField1;
+    private JLabel muayeneortamiLabel;
+    private JTextField muayeneortamitextField1;
+    private JLabel miknatisgiderimiLabel;
+    private JTextField miknatisgiderimitextField1;
+    private JLabel isilislemLabel;
+    private JTextField isilislemtextField1;
+    private JLabel yuzeysicakligiLabel;
+    private JTextField yuzeysicakligitextField1;
+    private JLabel isikcihaztanimiLabel;
+    private JTextField isikcihaztanimitextField1;
 
     public static Connection connect=null;
     public static Statement statement=null;
@@ -39,9 +59,19 @@ public class ekipman_bilgileri extends JFrame {
                 String a=akimtipitextField1.getText();
                 String b=yuzeytextField1.getText();
                 String c=kaldirmatestitarihitextField1.getText();
+                String d=kutupmesafesitextField1.getText();
+                String p=miknatislamateknigitextField1.getText();
+                String f=uvisiksiddetitextField1.getText();
+                String g=isikmesafesitextField1.getText();
+                String h=isiksiddetitextField1.getText();
+                String ı=muayeneortamitextField1.getText();
+                String i=miknatisgiderimitextField1.getText();
+                String j=isilislemtextField1.getText();
+                String k=yuzeysicakligitextField1.getText();
+                String l=isikcihaztanimitextField1.getText();
 
                 try {
-                    statement.executeUpdate("INSERT INTO ekipman_bilgileri VALUES ('" + x + "','" + y + "','" + z + "','"+ a +"','"+ b +"','"+ c +"')");
+                    statement.executeUpdate("INSERT INTO ekipman_bilgileri VALUES ('" + x + "','" + y + "','" + z + "','"+ a +"','"+ b +"','"+ c +",'"+d+"'p'"+"','"+f+"','"+g+"','"+h+"','"+ı+"','"+i+"','"+j+"','"+k+"','"+l+"')");
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
@@ -58,6 +88,16 @@ public class ekipman_bilgileri extends JFrame {
                 String a=akimtipitextField1.getText();
                 String b=yuzeytextField1.getText();
                 String c=kaldirmatestitarihitextField1.getText();
+                String d=kutupmesafesitextField1.getText();
+                String p=miknatislamateknigitextField1.getText();
+                String f=uvisiksiddetitextField1.getText();
+                String g=isikmesafesitextField1.getText();
+                String h=isiksiddetitextField1.getText();
+                String ı=muayeneortamitextField1.getText();
+                String i=miknatisgiderimitextField1.getText();
+                String j=isilislemtextField1.getText();
+                String k=yuzeysicakligitextField1.getText();
+                String l=isikcihaztanimitextField1.getText();
                 try {
 
                     statement.executeUpdate("DELETE FROM ekipman_bilgileri where cihaz='"+x+"'");
@@ -77,8 +117,18 @@ public class ekipman_bilgileri extends JFrame {
                 String a=akimtipitextField1.getText();
                 String b=yuzeytextField1.getText();
                 String c=kaldirmatestitarihitextField1.getText();
+                String d=kutupmesafesitextField1.getText();
+                String p=miknatislamateknigitextField1.getText();
+                String f=uvisiksiddetitextField1.getText();
+                String g=isikmesafesitextField1.getText();
+                String h=isiksiddetitextField1.getText();
+                String ı=muayeneortamitextField1.getText();
+                String i=miknatisgiderimitextField1.getText();
+                String j=isilislemtextField1.getText();
+                String k=yuzeysicakligitextField1.getText();
+                String l=isikcihaztanimitextField1.getText();
                 try{
-                    statement.executeUpdate("UPDATE ekipman_bilgileri set tasiyiciortam='"+y+"', muayenebolgesi='"+z+"',akimtipi='"+a+"',yuzey='"+b+"',kaldirmatestitarihi='"+c+"' where cihaz='"+x+"'");
+                    statement.executeUpdate("UPDATE ekipman_bilgileri set tasiyiciortam='"+y+"', muayenebolgesi='"+z+"',akimtipi='"+a+"',yuzey='"+b+"',kaldirmatestitarihi='"+c+"',kutupmesafesi='"+d+"',miknatislamateknigi='"+p+"',uvisiksiddeti='"+f+"',isikmesafesi='"+g+"',isiksiddeti='"+h+"',muayeneortami='"+ı+"',miknatisgiderimi='"+i+"',isilislem='"+j+"',yuzeysicakligi='"+k+"',isikcihaztanimi='"+l+"' where cihaz='"+x+"'");
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
@@ -105,7 +155,17 @@ public class ekipman_bilgileri extends JFrame {
                 String akimtipi=resultSet.getString(4);
                 String yuzey=resultSet.getString(5);
                 String kaldirmatestitarihi=resultSet.getString(6);
-                System.out.println(cihaz + " " + tasiyiciortam + " " +muayenebolgesi+" "+ akimtipi+" "+yuzey+" "+kaldirmatestitarihi);
+                String kutupmesafesi=resultSet.getString(7);
+                String miknatislamateknigi=resultSet.getString(8);
+                String uvisiksiddeti=resultSet.getString(9);
+                String isikmesafesi=resultSet.getString(10);
+                String isiksiddeti=resultSet.getString(11);
+                String muayeneortami=resultSet.getString(12);
+                String miknatisgiderimi=resultSet.getString(13);
+                String isilislem=resultSet.getString(14);
+                String yuzeysicakligi=resultSet.getString(15);
+                String isikcihaztanimi=resultSet.getString(16);
+                System.out.println(cihaz + " " + tasiyiciortam + " " +muayenebolgesi+" "+ akimtipi+" "+yuzey+" "+kaldirmatestitarihi+" "+kutupmesafesi+" "+miknatislamateknigi+" "+uvisiksiddeti+" "+isikmesafesi+" "+isiksiddeti+" "+muayeneortami+" "+miknatisgiderimi+" "+isilislem+" "+yuzeysicakligi+" "+isikcihaztanimi);
             }
         } catch (SQLException e) {
             e.printStackTrace();
