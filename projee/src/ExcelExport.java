d94df3fe1da2a55e27908f0bcf010fe2b36fd038
mucahitkosgen <e160503138@stud.tau.edu.tr>
2020-06-19 23:48:22 +0300

    deneme

--- projee/src/ExcelExport.java
+++ projee/src/ExcelExport.java
@@ -1,50 +1,76 @@
-import java.io.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.*;
-import java.text.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.*;
+public class ExcelExport {
 
-import javax.swing.*;
 
-public class AdvancedDb2ExcelExporter {
+    private static Object CellStyle;
+
+
+
     public static void main(String[] args) {
-        AdvancedDb2ExcelExporter exporter = new AdvancedDb2ExcelExporter();
-        exporter.export("yonetici");
-        exporter.export("muayene");
-        exporter.export("musterii");
-        exporter.export("ekipman_bilgileri");
-        exporter.export("muayenesonucları");
-        exporter.export("yonetici");
-        
-        
-    }
-    private String getFileName(String baseName) {
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-        String dateTimeInfo = dateFormat.format(new Date());
-        return baseName.concat(String.format("yonetici.xlsx", dateTimeInfo));}
+        ExcelExport exporter = new ExcelExport();
 
-    void export(String yonetici) {
+
+        exporter.export("onay");
+        exporter.export("degerlendiren");
+        exporter.export("operator");
+        exporter.export("musteri");
+        exporter.export("ekipman_bilgileri");
+        exporter.export("muayene");
+        exporter.export("muayenesonucları");
+
+
+
+    }
+
+
+
+
+
+    private static String getFileName(String baseName) {
+       DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String dateTimeInfo = dateFormat.format(new Date());
+        return baseName.concat(String.format("proje.xlsx", dateTimeInfo));}
+
+    static void export(String proje) {
         String url = "jdbc:mysql://localhost:3306/connect_mysql_database?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey";
         String user = "root";
         String pass = "Kule1845";
-        String excelFilePath = getFileName(yonetici.concat("_Export"));
+     String excelFilePath = getFileName(proje.concat("_Export"));
+
         try (Connection connection = DriverManager.getConnection(url, user, pass)) {
-            String sql = "SELECT * FROM ".concat(yonetici);
+           String sql = "SELECT * FROM ".concat(proje);
+
+
 
             Statement statement = connection.createStatement();
 
             ResultSet result = statement.executeQuery(sql);
 
+
             XSSFWorkbook workbook = new XSSFWorkbook();
-            XSSFSheet sheet = workbook.createSheet(yonetici);
+
+            XSSFSheet sheet = workbook.createSheet(proje);
 
             writeHeaderLine(result, sheet);
 
             writeDataLines(result, workbook, sheet);
 
-            FileOutputStream outputStream = new FileOutputStream(excelFilePath);
+
+
+           FileOutputStream outputStream = new FileOutputStream(excelFilePath);
             workbook.write(outputStream);
             workbook.close();
 
@@ -59,7 +85,8 @@ public class AdvancedDb2ExcelExporter {
         }
     }
 
-    private void writeDataLines(ResultSet result, XSSFWorkbook workbook, XSSFSheet sheet)throws SQLException {
+
+    private static void writeDataLines(ResultSet result, XSSFWorkbook workbook, XSSFSheet sheet)throws SQLException {
         ResultSetMetaData metaData = result.getMetaData();
         int numberOfColumns = metaData.getColumnCount();
 
@@ -72,6 +99,7 @@ public class AdvancedDb2ExcelExporter {
                 Object valueObject = result.getObject(i);
 
                 Cell cell = row.createCell(i - 1);
+
 
                 if (valueObject instanceof Boolean)
                     cell.setCellValue((Boolean) valueObject);
@@ -89,30 +117,40 @@ public class AdvancedDb2ExcelExporter {
         }
     }
 
-    private void formatDateCell(XSSFWorkbook workbook, Cell cell) {
+
+    private static void formatDateCell(XSSFWorkbook workbook, Cell cell) {
+
         CellStyle cellStyle = workbook.createCellStyle();
+
+
         CreationHelper creationHelper = workbook.getCreationHelper();
-        cellStyle.setDataFormat(creationHelper.createDataFormat().getFormat("yyyy-MM-dd HH:mm:ss"));
+        cellStyle.setDataFormat(creationHelper.createDataFormat().getFormat("yyyy-MM-dd_HH-mm-ss"));
+
         cell.setCellStyle(cellStyle);
     }
 
 
 
-    private void writeHeaderLine(ResultSet result, XSSFSheet sheet) throws SQLException {
+    private static void writeHeaderLine(ResultSet result, XSSFSheet sheet) throws SQLException {
         ResultSetMetaData metaData = result.getMetaData();
         int numberOfColumns = metaData.getColumnCount();
 
         Row headerRow = sheet.createRow(0);
 
 
-        // exclude the first column which is the ID field
-        for (int i = 1; i <= numberOfColumns; i++) {
-            String columnName = metaData.getColumnName(i);
-            Cell headerCell = headerRow.createCell(i - 1);
-            headerCell.setCellValue(columnName);
+
+         for (int i = 1; i <= numberOfColumns; i++)
+           {
+
+                    String columnName = metaData.getColumnName(i);
+                    Cell headerCell = headerRow.createCell(i - 1);
+                    headerCell.setCellValue(columnName);
         }
     }
-    }
+
+
+    
+}
 
 
 
